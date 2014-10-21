e22a44bb1853aa2c4b61db4d36b591c222c0b9ae
adewaleafolabi <adewaleafolabi@gmail.com>
2014-10-21 23:32:20 +0100

    Create TableData.java
    initial commit

---
+++ TableData.java
@@ -0,0 +1,10 @@
+package JavaUtils;
+
+/**
+ * Created by wale on 10/21/14.
+ * This interface must be implemented by Table.java List items
+ */
+public interface TableData {
+
+    String[] toArray();
+}
