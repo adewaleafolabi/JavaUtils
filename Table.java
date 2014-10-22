4f72bb67594c13a9018684a43eea89745559982e
adewaleafolabi <adewaleafolabi@gmail.com>
2014-10-22 22:51:44 +0100

    Update Table.java
    1. Fixed - Table Header and Body now has the same number of items
    2. TAGS were introduced to make code look cleaner

--- Table.java
+++ Table.java
@@ -9,43 +9,58 @@ import java.util.Map;
 
 /**
  * Created by wale on 10/21/14.
- * Class generates an HTML table from 
- * a List of Objects that implements 
+ * Class generates an HTML table from
+ * a List of Objects that implements
  * the TableData interface
  */
 public class Table {
-    private String [] header;
-    private List<? extends TableData> tableData;
-    private List<String[]> tableDataX = new ArrayList<>();
-    private Map<String,String> htmlArgs;
+
+    private final String[] header;
+    private final List<? extends TableData> tableData;
+    private final List<String[]> tableDataX = new ArrayList<>();
+    private final Map<String, String> htmlArgs;
+    private final String TAG_NEW_LINE = "\n";
+    private final String TAB = "\t";
+    private final String TAG_TBODY_OPEN = "<tbody>";
+    private final String TAG_TBODY_CLOSE = "</tbody>";
+    private final String TAG_TH_OPEN = "<thead>";
+    private final String TAG_TH_CLOSE = "</thead>";
+    private final String TAG_TR_OPEN = "<tr>";
+    private final String TAG_TR_CLOSE = "</tr>";
+    private final String TAG_TD_OPEN = "<td>";
+    private final String TAG_TD_CLOSE = "</td>";
+    private final String TAG_NBSPC = "&nbsp;";
+    private int tdCount = 0;
 
 
-    public Table(String[] header, Map<String,String> htmlArgs, List<? extends TableData> tableData) {
+    public Table(String[] header, Map<String, String> htmlArgs, List<? extends TableData> tableData) {
         this.header = header;
-        this.htmlArgs=htmlArgs;
+        this.htmlArgs = htmlArgs;
         this.tableData = tableData;
+        this.tdCount = this.header.length;
 
         prepData();
     }
 
     /**
-     *Prepares tableData as a List of arrays
+     * Prepares tableData as a List of arrays
      */
-    private void prepData(){
+    private void prepData() {
 
-        for(int i =0;i<tableData.size();i++){
+        for (int i = 0; i < tableData.size(); i++) {
             tableDataX.add(tableData.get(i).toArray());
         }
     }
 
     /**
      * Generates the table tag and the html arguments
+     *
      * @return String
      */
-    private String createOpening(){
+    private String createOpening() {
         StringBuilder builder = new StringBuilder();
         builder.append("<table ");
-        for(Map.Entry<String,String> entry : htmlArgs.entrySet()){
+        for (Map.Entry<String, String> entry : htmlArgs.entrySet()) {
             builder.append(entry.getKey());
             builder.append("='");
             builder.append(entry.getValue());
@@ -59,42 +74,75 @@ public class Table {
     }
 
     /**
-     *Generates the table head section
+     * Generates the table head section
+     *
      * @return String
      */
-    private String createHeader(){
+    private String createHeader() {
 
         StringBuilder builder = new StringBuilder();
-        builder.append("\n\t<thead>\n\t<tr>\n");
-        for(String item : this.header){
-            builder.append("\t<td>");
-            builder.append(item);
-            builder.append("</td>\n");
+        builder.append(TAG_NEW_LINE)
+                .append(TAB)
+                .append(TAG_TH_OPEN)
+                .append(TAG_NEW_LINE)
+                .append(TAB)
+                .append(TAG_TR_OPEN)
+                .append(TAG_NEW_LINE);
+
+
+        for (String item : this.header) {
+            builder.append(TAB)
+                    .append(TAG_TD_OPEN)
+                    .append(item)
+                    .append(TAG_TD_CLOSE)
+                    .append(TAG_NEW_LINE);
 
         }
-        builder.append("\t</tr>\n</thead>\n");
+        builder.append(TAB)
+                .append(TAG_TR_CLOSE)
+                .append(TAG_NEW_LINE)
+                .append(TAG_TH_CLOSE)
+                .append(TAG_NEW_LINE);
         return builder.toString();
     }
 
     /**
-     *Generates the table body section
+     * Generates the table body section
+     *
      * @return String
      */
-    private String createBody(){
+    private String createBody() {
         StringBuilder builder = new StringBuilder();
-        builder.append("\n");
-        builder.append("<tbody>\n");
+        builder.append(TAG_NEW_LINE)
+                .append(TAG_TBODY_OPEN)
+                .append(TAG_NEW_LINE);
 
-        for(String [] item : tableDataX){
-            builder.append("\t<tr>\n");
-            for(String tdItem : item){
-                builder.append("\t\t<td>");
-                builder.append(tdItem);
-                builder.append("</td>\n");
+        for (String[] item : tableDataX) {
+            builder.append(TAB)
+                    .append(TAG_TR_OPEN)
+                    .append(TAG_NEW_LINE);
+
+            //Using the length of the header array in order
+            // to ensure that the TD generated matches
+            // the column size
+
+            for (int i = 0; i < header.length; i++) {
+                builder.append(TAB)
+                        .append(TAB)
+                        .append(TAG_TD_OPEN);
+                try {
+                    builder.append(item[i]);
+                } catch (Exception exp) {
+                    builder.append(TAG_NBSPC);
+                }
+                builder.append(TAG_TD_CLOSE)
+                        .append(TAG_NEW_LINE);
             }
-            builder.append("\t</tr>\n");
+            builder.append(TAB)
+                    .append(TAG_TR_CLOSE)
+                    .append(TAG_NEW_LINE);
         }
-        builder.append("</tbody>\n");
+        builder.append(TAG_TBODY_CLOSE).append(TAG_NEW_LINE);
 
         return builder.toString();
 
@@ -102,21 +150,23 @@ public class Table {
 
     /**
      * Generates the Tables closing tags
+     *
      * @return String
      */
-    private String createClosing(){
+    private String createClosing() {
         return "</table>";
     }
 
     /**
      * Renders the raw table HTML
+     *
      * @return String
      */
-    public String render(){
+    public String render() {
 
-        return createOpening() + createHeader() + createBody() +createClosing();
+        return createOpening() + createHeader() + createBody() + createClosing();
     }
 
 
-
 }
+
